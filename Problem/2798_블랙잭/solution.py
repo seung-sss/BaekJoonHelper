e2d43d656db9c7fd9fb6e968d3e3135f28286a39
seung-sss <sjun804@gmail.com>
2021-11-26 01:38:23 +0900

    add problem number 2798

--- Problem/2798_블랙잭/solution.py
+++ Problem/2798_블랙잭/solution.py
@@ -1,5 +1,16 @@
+# 브루트포스 문제로 완전 탐색이 필요!
+from itertools import combinations
+
 def solution():
+	n, m = map(int, input().split())
+	card = list(map(int, input().split()))
 
-	inputString = input()
+	result = 0
+	for c in combinations(card, 3):
+		tmp = sum(c)
+		if tmp <= m and tmp > result:
+			result = tmp
 
-	print(inputString)
+	print(result)
+
+solution()
